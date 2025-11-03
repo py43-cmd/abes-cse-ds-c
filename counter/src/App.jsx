b69bd7bc7471b08dececdb17b3b7f8b931ffe982
priyanshu <py431035gmail.com>
2025-11-03 15:37:18 +0530

    your update message

---
+++ counter/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1> Count {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </>
+  );
+}
+
+export default App;
